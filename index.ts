9fdf5bf7ad668e0d2d4ff51bd25644c1defbdcfb
IshimweArlene <ishimwearlene74@gmail.com>
2025-11-23 21:18:21 +0200

    changes

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
 
+require("dotenv").config();
 const app = express();
 
+app.use("/api/auth", authRoutes);
 app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log(" MongoDB connected"))
-  .catch((err) => console.error("DB error:", err));
+  .catch((err:any) => console.error("DB error:", err));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
